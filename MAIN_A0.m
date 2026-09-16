ad452471f9c7a1c3c7f129e95009b9126a94b767
CompBiosUnifi <compbios@dinfo.unifi.it>
2026-09-16 10:03:35 +0200

    Update comments for clarity and translation

---
+++ MAIN_A0.m
@@ -0,0 +1,12 @@
+%% L0 - Open and discuss the .edf file
+clear all
+close all
+clc
+
+%open the edf file SN001
+
+%read the edf info SN001
+
+%Punto di discussione, riesco a risalire e leggere tutte le informazioni necessarie dei file salvati?
+%Can I access and read all the necessary information from the saved files?
+
