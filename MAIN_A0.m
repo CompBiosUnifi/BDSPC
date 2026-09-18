b2776d43662d62cfdca89e0d004d08dc7f1b0dee
CompBiosUnifi <compbios@dinfo.unifi.it>
2026-09-18 23:01:18 +0200

    Enhance comments for EDF file processing
    Added comments and structure for discussing EDF file handling and visualization.

--- MAIN_A0.m
+++ MAIN_A0.m
@@ -4,9 +4,26 @@ close all
 clc
 
 %open the edf file SN001
+edfFile = 'SN001.edf';
+edfInfo = edfinfo(edfFile);
 
-%read the edf info SN001
+% Read the data from the EDF file
+edfData = edfread(edfFile);
 
 %Punto di discussione, riesco a risalire e leggere tutte le informazioni necessarie dei file salvati?
 %Can I access and read all the necessary information from the saved files?
+
+%%% >--------------------------------------------------------------------------------<
+%% L1 - Open and discuss the content, and visualize the entire dataset.
+%Different ways to accomplish the same thing.
+
+%Open and read the .edf file used in L0.
+
+%Define a loop to scan its components and reconstruct the signal.
+
+%Turn this operation into a function and test calling it on another column.
+
+%Visualize multiple signals, taking into account their associated sampling frequencies.
+
+%Provide the possibility of performing basic operations on the signal(s).
 
